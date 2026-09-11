32d27b7bad913a4678d7b3ad8e53d8704508f129
VidyaShree2007 <vidyashrrehl@gmail.com>
2026-09-10 21:36:25 -0500

    FastAPI Day 2

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# Home
 @app.get("/")
 def home():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Student API is running"}
 
 
-@app.get("/about")
-def about():
+# 1. Path parameter
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
     return {
-        "name": "Bindu",
-        "learning": "FastAPI",
-        "day": 1
+        "student_id": student_id
+    }
+
+
+# 2. Query parameters
+@app.get("/students")
+def search_student(name: str, age: int):
+    return {
+        "name": name,
+        "age": age
+    }
+
+
+# 3. Path + Query parameter
+@app.get("/students/{student_id}/course")
+def get_course(student_id: int, course: str):
+    return {
+        "student_id": student_id,
+        "course": course
     }
